9ea9b64bf446faa4a6255c4226d501ffabe597e0
BigSamu <valdesgutierrez@gmail.com>
2023-04-11 20:55:46 -0400

    updating PortfolioItem.jsx component

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+
+import { FaExternalLinkAlt, FaGithub } from 'react-icons/fa';
 import { getTechnologyIcon } from '../utils/TechnologyIcon.jsx';
 
-const PortfolioItem = ({project}) => {
-
+const PortfolioItem = ({ project }) => {
   return (
     <div className="flex flex-col bg-white dark:bg-gray-900 shadow-md rounded-md p-4">
       <img
@@ -10,12 +11,8 @@ const PortfolioItem = ({project}) => {
         alt={project.title}
         className="w-full h-36 md:h-48 object-cover rounded-md mb-4"
       />
-      <h3 className="text-lg font-bold">
-        {project.title}
-      </h3>
-      <p className="text-sm italic mb-2">
-        {project.type}
-      </p>
+      <h3 className="text-lg font-bold">{project.title}</h3>
+      <p className="text-sm italic mb-2">{project.type}</p>
       <p className="grow">{project.details}</p>
       <ul className="text-sm my-5">
         {project.stack.map((technology, index) => (
@@ -26,32 +23,38 @@ const PortfolioItem = ({project}) => {
           </li>
         ))}
       </ul>
-      { project.repo 
-        ?
-        <a
-          href={project.repo}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500 hover:text-blue-600 dark:text-blue-300 dark:hover:text-blue-400"
-        >
-          View Project
-        </a>
-        :
-        <span className="italic">Project under NDA</span>
-      }
-      { project.demo 
-        ?
-        <a
-          href={project.demo}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500 hover:text-blue-600 dark:text-blue-300 dark:hover:text-blue-400"
-        >
-          Demo
-        </a>
-        :
-        ""
-      }
+      <div className="flex space-x-3">
+        {project.repo ? (
+          <a
+            href={project.repo}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:text-blue-600 dark:text-blue-300 dark:hover:text-blue-400"
+          >
+            <span className="flex items-center space-x-1">
+              <FaGithub />
+              <span className="underline">Github</span>
+            </span>
+          </a>
+        ) : (
+          <span className="italic">Project under NDA</span>
+        )}
+        {project.demo ? (
+          <a
+            href={project.demo}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:text-blue-600 dark:text-blue-300 dark:hover:text-blue-400"
+          >
+            <span className="flex items-center space-x-1">
+              <FaExternalLinkAlt />
+              <span className="underline">Demo</span>
+            </span>
+          </a>
+        ) : (
+          ''
+        )}
+      </div>
     </div>
   );
 };
